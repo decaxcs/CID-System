87001bc7cf27a207de36d205ada099768b759124
Fhranz Joseph Abella Benosa <97322613+decaxcs@users.noreply.github.com>
2024-03-20 14:56:19 +0800

    Update

--- Frontdesk/PAGES/front_desk.php
+++ Frontdesk/PAGES/front_desk.php
@@ -22,23 +22,41 @@
 <body>
     <div id="front_desk_page">
         <div id="side_blue_box">
+            <div id="logo_container">
+                <img id="logo_image" src="../../Assets/Images/logo.png" alt="" srcset="">
+                <div id="logo_text_container">
+                    <p id="company_name">Techy David</p>
+                    <p id="company_sub_name">IT Solutions</p>
+                </div>
+            </div>
+            <div id="time_date_container">
+                <div class="card calendar_card">
+                    <div class="card-body calendar_body">
+                    </div>
+                </div>
+                <div id="time">
+                    <p id="current-time"></p>
+                </div>
+            </div>
         </div>
+
         <div id="front_desk_container">
-            <div id="top_gray_box">
-                <p id="dashboard_title_text">DASHBOARD</p>
+            <div id="header">
+
             </div>
             <div id="dashboard_container">
+                <p id="dashboard_title_text">DASHBOARD</p>
                 <div id="dashboard_greeting_container">
                     <p id="greetings">Good Morning, <br>
                         <span id="front_desk_name">Lorem!<span>
                     </p>
-                    <button type="button" class="btn btn-primary btn_create" data-bs-toggle="modal"
+                    <!-- <button type="button" class="btn btn-primary btn_create" data-bs-toggle="modal"
                         data-bs-target="#add_CID_modal">
                         CREATE
-                    </button>
+                    </button> -->
                 </div>
                 <div id="dashboard_card_container">
-                    <div class="card">
+                    <div class="card stats_card">
                         <div class="card-body">
                             <div class="info_container ">
                                 <div>
@@ -52,11 +70,10 @@
                                             width="62px" height="62px">
                                         </iconify-icon>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
-                        <div class="card-body more_info" onclick="navigateToLink('#link')">
+                        <div class="card-body more_info" onclick="navigateToLink('https:google.com')">
                             <p class="me-2">More Info</p>
                             <iconify-icon class="more_info_icon" icon="icomoon-free:circle-right"></iconify-icon>
                         </div>
@@ -66,11 +83,9 @@
         </div>
     </div>
 
-    <script>
-        function navigateToLink(url) {
-            window.location.href = url;
-        }
-    </script>
+
+    <script src="../JS/front_desk.js"></script>
+
     <?php include "add_cid_modal.php"; ?>
 </body>
 
